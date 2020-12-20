577e979149a3ec7a6fcbe9aa550143029f53f53b
Kiet <72509022+wwwKiet@users.noreply.github.com>
2020-12-20 09:12:02 +0300

    2020-12-20 second commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "post1234",
-  DB: "tags",
+  HOST: "ec2-52-203-165-126.compute-1.amazonaws.com",
+  USER: "frvpulqsxbpqhf",
+  PASSWORD: "95f2b4490927047754af96fa3d64b78cabcd72bbbf5bf9f464f99282348d4538",
+  DB: "de0qj8puceqmhc",
   PORT: 5432
 };
